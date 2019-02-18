47fb583ce6d798bef895bf2dc18fc4cf7a00594d
tianxin <tianxin.shi@shanbay.com>
2019-02-18 23:25:05 +0800

    Updates

---
+++ precache-manifest.e71f697354a7da8c3556a19f05f21820.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "759a8d9d0410311681b7",
+    "url": "/static/js/main.759a8d9d.chunk.js"
+  },
+  {
+    "revision": "ce16b479370c47e539d5",
+    "url": "/static/js/2.ce16b479.chunk.js"
+  },
+  {
+    "revision": "759a8d9d0410311681b7",
+    "url": "/static/css/main.a725f03a.chunk.css"
+  },
+  {
+    "revision": "27b587a990aa9a79a7b3284211342979",
+    "url": "/index.html"
+  }
+];
